789d847e4d70986866833e68a2952a71a5582702
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-07-13 19:26:22 +0300

    create files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// 1  
+
+var num = 10;
+var numF = '10';
+console.log ( num + numF );
+console.log ( num - numF );
+console.log ( num / numF );
+console.log ( num * numF );
+console.log ( num > numF );
+console.log ( num < numF );
+console.log ( num >= numF );
+console.log ( num <= numF );
+console.log ( num == numF );
+console.log ( num != numF );
+console.log ( num !== numF );
+console.log ( num === numF );
